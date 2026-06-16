54280228145bc74df3c212258234f6068d924aa5
d11416146 <d11416146@gs.takming.edu.tw>
2026-06-16 15:27:52 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Student
+{
+	public string[] Names;
+	public Student(int size)
+	{
+		Names= new string[size];
+		for(int i = 0; i < size; i++)
+		{
+			Names[i] = "AAA";
+		}
+	}
+	public string this[int pos]
+	{
+		get
+		{
+			return this.Names[pos];
+		}
+		set
+		{
+			this.Names[pos] = value;
+		}
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+
+namespace class0616_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s1 = new Student(5);
+
+            s1[0] = "CCC";
+            s1[1] = "BBB";
+            s1[2] = "EEE";
+
+            Console.WriteLine(s1[0]);
+            Console.WriteLine(s1[1]);
+            Console.WriteLine(s1[2]);
+            Console.WriteLine(s1[3]);
+            Console.WriteLine(s1[4]);
+        }    
+    }
+}
